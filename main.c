456ca4e0c2508f439605e614fbb42a8f842f916d
adam <flammino@gmail.com>
2018-03-29 17:07:16 -0400

    need to figure out segfault at line 59, workaround in place for now. Working otherwise.

--- main.c
+++ main.c
@@ -10,22 +10,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
+#include <signal.h>
 
-#define THREADS 3
+#define THREADS 10
 
-int lock = 0;
+bool lock = false;
 int x;
 int y;
-int result1;
-int result2;
-int result3;
+int result;
 int totalLoops = 3000;
 int totalLoopsCompleted = 0;
 
-int waiting [THREADS];
+bool waiting [THREADS];
+int timesRan[THREADS];
 
-int compareAndSwap(int *lock, int expected, int newValue){
-    int temp = *lock;
+bool compareAndSwap(bool *lock, bool expected, bool newValue){
+    bool temp = *lock;
     if (*lock == expected){ // swaps to allow for locking
         *lock = newValue;
     }
@@ -34,63 +34,71 @@ int compareAndSwap(int *lock, int expected, int newValue){
 
 
 
-void * first(){
+void * first(void * t){
+    int thread = *(( int *)t);
     int i = 0;
+    int j = -1;
+    bool waitVal = false;
     for(totalLoopsCompleted; totalLoopsCompleted < totalLoops; totalLoopsCompleted++) {
-        i++;
-        printf("\nThread one starting for the %i time\n", i);
-        while (compareAndSwap(&lock, 0, 1) == 0) // spinlock
-        x = 5;
-        y = 2;
-        result1 = x + y;
-        lock = 0; // Unlocks
-        printf("Thread 1 thinks 5 + 2 = %i\n", result1);
-        usleep(rand() % 100);
-    }
-    result1 = i;
-}
-
-void * second(){
-    int i = 0;
-    for(totalLoopsCompleted; totalLoopsCompleted < totalLoops; totalLoopsCompleted++) {
-        i++;
-        printf("\nThread two starting for the %i time\n", i);
-        while (compareAndSwap(&lock, 0, 1) == 0) // spinlock
-        x = 3;
+        waiting[thread] = true; // Thread is waiting to start once it hits this point
+        while (!(compareAndSwap(&lock, false, true)) && waiting[thread]) //stalls until lock or waiting is false
+            // (waiting false means turn is up)
+        x = thread;
         y = 7;
-        result2 = x - y;
-        lock = 0; // Unlocks
-        printf("Thread 2 thinks 3 - 7 = %i\n", result2);
+        result = x + y;
+        if (x + y != result){ // Checks to make sure variables weren't overwritten during lock
+            printf("\n\nMutual Exclusion Error! Thread %i, loop %i.",
+                   thread, totalLoopsCompleted);
+            exit(1);
+        };
+        while(j < 0 || j >= THREADS){ // I put this in a while loop since I kept getting a weird segfault around loop 15, line 59
+            j = thread + 1;
+            j %= THREADS; // starts at next thread
+            waitVal = waiting[j];
+        }
+        while ((j + 1) % THREADS != i && !waitVal){ // Cycles through looking for next waiting thread
+            j = j + 1 % THREADS;
+            waitVal = waiting[j]; // if not for the segfault, waitVal could be omitted and just test waiting[j] directly in the while
+        }
+        if (j == thread){ // No waiting threads, open the lock
+            lock = false;
+        }
+        else{
+            waiting[j] = false; // Allow next waiting thread to continue
+        }
+        i++;
+        timesRan[thread] = i; // for ensuring all threads are running evenly
         usleep(rand() % 100);
     }
-    result2 = i;
 }
 
-void * third(){
-    int i = 0;
-    for(totalLoopsCompleted; totalLoopsCompleted < totalLoops; totalLoopsCompleted++) {
-        i++;
-        printf("\nThread three starting for the %i time\n", i);
-        while (compareAndSwap(&lock, 0, 1) == 0) // spinlock
-        x = 9;
-        y = 6;
-        result3 = x * y;
-        lock = 0; // Unlocks
-        printf("Thread 3 thinks 9 * 6 = %i", result3);
-        usleep(rand() % 100);
-    }
-    result3 = i;
-}
 
 int main() {
-    int threads = 2;
     srand(time(NULL));
-    pthread_t tid[threads];
-    pthread_create(&tid[0], NULL, &first, NULL);
-    pthread_create(&tid[1], NULL, &second, NULL);
-    pthread_create(&tid[2], NULL, &third, NULL);
-    pthread_join(tid[0],NULL);
-    pthread_join(tid[1],NULL);
-    pthread_join(tid[2],NULL);
+    pthread_t tid[THREADS];
+
+    for (int i = 0; i < THREADS; i++){
+        waiting[i] = false; // no threads are waiting before being called
+    }
+
+    for (int i = 0; i < THREADS; i++){
+        int *arg = malloc(sizeof(*arg));// needed to keep memory straight between threads
+        if ( arg == NULL ) { // arg setup from https://stackoverflow.com/questions/19232957/pthread-create-passing-an-integer-as-the-last-argument
+            fprintf(stderr, "Couldn't allocate memory for thread arg.\n");
+            exit(EXIT_FAILURE);
+        }
+        *arg = i;
+        pthread_create(&tid[i], NULL, &first, arg);
+    }
+
+    for (int i = 0; i < THREADS; i++){ // Waits for threads to finish
+        pthread_join(tid[i], NULL);
+    }
+
+    for (int i = 0; i < THREADS; i ++){
+        float percentageRan = 100.0*timesRan[i] / totalLoopsCompleted;
+        printf("\nThread %i ran %f percent of the time.\n", i, percentageRan);
+    }
+    printf("%i loops completed, no errors.", totalLoopsCompleted);
     return 0;
 }
